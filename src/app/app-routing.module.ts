001aa5258db88850ce760a0e7a38d05d7880994e
fimovol <fimovolpe@gmail.com>
2023-01-17 09:27:11 -0500

    cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './principal/contacto/contacto.component';
+import { HomeComponent } from './principal/home/home.component';
+import { ProductosComponent } from './principal/productos/productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"contacto",component:ContactoComponent},
+  {path:"productos",component:ProductosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
